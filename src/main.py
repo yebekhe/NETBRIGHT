de759fea1895762cb810e00592a4254c4036653b
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-05-03 13:43:33 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,13 @@ import subprocess
 import json
 import random
 import time
+import socket
+import threading
+from concurrent.futures import ThreadPoolExecutor
+import os
+import sys
+import argparse
+import logging
 from time import sleep
 from kivy.clock import Clock
 from kivy.network.urlrequest import UrlRequest
@@ -14,6 +21,11 @@ from kivy.metrics import dp
 from kivy.lang.builder import Builder
 from kivy.properties import StringProperty
 from kivymd.uix.list import OneLineIconListItem
+
+if os.name == 'posix':
+    import resource
+    soft_limit, hard_limit = resource.getrlimit(resource.RLIMIT_NOFILE)
+    resource.setrlimit(resource.RLIMIT_NOFILE, (soft_limit, hard_limit))
 
 KV = '''
 MDFloatLayout:
@@ -38,17 +50,6 @@ MDFloatLayout:
         text: "Select Your Operator"
         on_release: app.menu.open()
         pos_hint: {'y': .6 , "center_x": .5}
-        size_hint_x: .6
-
-    MDTextField:
-        id: cloudflare_ip_input
-        halign: 'center'
-        hint_text: "Enter Your Cloudflare IP"
-        helper_text: "Select Your Operator if you don't have any IP!"
-        helper_text_mode: "persistent"
-        opacity: 0
-        height: 0
-        pos_hint: {'y': .5 , "center_x": .5}
         size_hint_x: .6
 
     MDTextField:
@@ -89,7 +90,7 @@ class MainApp(MDApp):
         super().__init__(**kwargs)
         self.screen = Builder.load_string(KV)
         menu_items = [
-           {"text": "Manual", "on_release": lambda x="Manual": self.set_item(x) , "viewclass": "IconListItem", "height": dp(56)},
+           {"text": "Automatic", "on_release": lambda x="Automatic": self.set_item(x), "viewclass": "IconListItem", "height": dp(56)},
            {"text": "Hamrah-Aval", "on_release": lambda x="Hamrah-Aval": self.set_item(x), "viewclass": "IconListItem", "height": dp(56)},
            {"text": "Irancell", "on_release": lambda x="Irancell": self.set_item(x), "viewclass": "IconListItem", "height": dp(56)},
            {"text": "Rightel", "on_release": lambda x="Rightel": self.set_item(x), "viewclass": "IconListItem", "height": dp(56)},
@@ -123,119 +124,165 @@ class MainApp(MDApp):
     def build(self):
         return self.screen
 
+    def send_data_in_fragment(self, data, sock):
+        for i in range(0, len(data), self.L_fragment):
+            fragment_data = data[i:i+self.L_fragment]
+            logging.debug(f'[SEND] {len(fragment_data)} bytes')
+            sock.sendall(fragment_data)
+            time.sleep(self.fragment_sleep)
+        logging.debug('[SEND] ----------finish------------')
+
+    def my_upstream(self, client_sock):
+        first_flag = True
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as backend_sock:
+            backend_sock.settimeout(self.my_socket_timeout)
+            while True:
+                try:
+                    if first_flag:
+                        first_flag = False
+                        time.sleep(self.first_time_sleep)
+                        data = client_sock.recv(16384)
+                        data_len = len(data)
+                        self.L_fragment = random.randint(15, data_len // 3)
+                        self.fragment_sleep = 0.0025974025974026 * self.L_fragment
+                        if data:
+                            backend_ip = self.get_next_backend_ip()
+                            print(f'Using backend IP: {backend_ip}')  # Print the selected backend IP
+                            backend_sock.connect((backend_ip, self.Cloudflare_port))
+                            thread_down = threading.Thread(target=self.my_downstream, args=(backend_sock, client_sock))
+                            thread_down.daemon = True
+                            thread_down.start()
+                            send_data_in_fragment(data, backend_sock)
+                        else:
+                            raise Exception('cli syn close')
+                    else:
+                        data = client_sock.recv(4096)
+                        if data:
+                            backend_sock.sendall(data)
+                        else:
+                            raise Exception('cli pipe close')
+                except Exception as e:
+                    logging.debug(f'[UPSTREAM] {repr(e)}')
+                    time.sleep(2)
+                    client_sock.close()
+                    return False
+
+    def my_downstream(self, backend_sock, client_sock):
+        first_flag = True
+        while True:
+            try:
+                if first_flag:
+                    first_flag = False
+                    data = backend_sock.recv(16384)
+                    if data:
+                        client_sock.sendall(data)
+                    else:
+                        raise Exception('backend pipe close at first')
+                else:
+                    data = backend_sock.recv(4096)
+                    if data:
+                        client_sock.sendall(data)
+                    else:
+                        raise Exception('backend pipe close')
+            except Exception as e:
+                logging.debug(f'[DOWNSTREAM] {repr(e)}')
+                time.sleep(2)
+                client_sock.close()
+                return False
+
     def set_item(self, text_item):
         self.screen.ids.operator_dropdown.set_item(text_item)
-        self.cloudflare_input = self.screen.ids.cloudflare_ip_input
-        if text_item == "Manual":
-            self.cloudflare_input.opacity = 1
-            self.cloudflare_input.height = dp(60)
-        else:
-            self.cloudflare_input.opacity = 0
-            self.cloudflare_input.height = 0
         self.menu.dismiss()
+
+    def get_next_backend_ip(self):
+        if self.user_operator == "AUTO":
+            self.OP = ['MCI', 'MTN', 'RTL', 'MKH']
+            self.chosen_OP = random.choice(self.OP)
+            selected_ip = self.choose_random_ip_with_operator(self.chosen_OP)
+        else :
+            selected_ip = self.choose_random_ip_with_operator(self.user_operator)
+        return selected_ip
+
+    def choose_random_ip_with_operator(self, operator_code):
+        response = UrlRequest("https://raw.githubusercontent.com/yebekhe/cf-clean-ip-resolver/main/list.json")
+        while not response.is_finished:
+            sleep(1)
+            Clock.tick()
+
+        json_data = response.result
+        data = json.loads(json_data)
+        matching_ips = []
+        for ipv4_address in data["ipv4"]:
+            if ipv4_address["operator"] == operator_code:
+                matching_ips.append(ipv4_address["ip"])
+        matching_count = len(matching_ips)
+        if matching_count > 0:
+            random_index = random.randint(0, matching_count - 1)
+            random_ip = matching_ips[random_index]
+            return random_ip
+        else:
+            return None
 
     def start_tunnel(self):
         if self.condition_of_tunnel == False :
              self.condition_of_tunnel = True
              self.screen.ids.start_button.text = 'Stop Tunnel!'
-             user_operator_full = self.screen.ids.operator_dropdown.current_item
-             operators = {
-                 "Manual": "manual",
-                 "Hamrah-Aval": "MCI",
-                 "Irancell": "MTN",
-                 "Rightel": "RTL",
-                 "Mokhaberat": "MKH",
-                 "HiWeb": "HWB",
-                 "AsiaTech": "AST",
-                 "Shatel": "SHT",
-                 "ParsOnline": "PRS",
-                 "MobinNet": "MBT",
-                 "Andishe-Sabz-Khazar": "ASK",
-                 "Respina": "RSP",
-                 "AfraNet": "AFN",
-                 "Zi-Tel": "ZTL",
-                 "Pishgaman": "PSM",
-                 "Araax": "ARX",
-                 "SamanTel": "SMT",
-                 "FanAva": "FNV",
-                 "DidebanNet": "DBN",
-                 "ApTel": "APT",
-                 "Fanap-Telecom": "FNP",
-                 "RayNet": "RYN"
-             }
-             user_operator = operators.get(user_operator_full)
-             if user_operator == "manual":
-                 self.Cloudflare_IP = self.screen.ids.cloudflare_ip_input.text
-             elif user_operator == "MCI":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("MCI")
-             elif user_operator == "MTN":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("MTN")
-             elif user_operator == "RTL":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("RTL")
-             elif user_operator == "MKH":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("MKH")
-             elif user_operator == "HWB":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("HWB")
-             elif user_operator == "AST":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("AST")
-             elif user_operator == "SHT":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("SHT")
-             elif user_operator == "PRS":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("PRS")
-             elif user_operator == "MBT":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("MBT")
-             elif user_operator == "ASK":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("ASK")
-             elif user_operator == "RSP":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("RSP")
-             elif user_operator == "AFN":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("AFN")
-             elif user_operator == "ZTL":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("ZTL")
-             elif user_operator == "PSM":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("PSM")
-             elif user_operator == "ARX":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("ARX")
-             elif user_operator == "SMT":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("SMT")
-             elif user_operator == "FNV":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("FNV")
-             elif user_operator == "DBN":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("DBN")
-             elif user_operator == "APT":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("APT")
-             elif user_operator == "FNP":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("FNP")
-             elif user_operator == "RYN":
-                 self.Cloudflare_IP = choose_random_ip_with_operator("RYN")
-             else:
-                 self.Cloudflare_IP = '162.159.36.93'
-             print ("Your Cloudflare IP :"+str(self.Cloudflare_IP))
-             self.server = subprocess.Popen(["python3","side_job.py",f"{self.screen.ids.local_port_input.text}",f"{self.Cloudflare_IP}",f"{self.screen.ids.config_port_input.text}"])
+             t = threading.Thread(target=self._start_tunnel)
+             t.daemon = True
+             t.start()
         else :
              self.condition_of_tunnel = False
              self.screen.ids.start_button.text = 'Start Tunnel!'
-             self.server.terminate()
+             print(f'Tunnel Stopped!')
 
-def choose_random_ip_with_operator(operator_code):
-    response = UrlRequest("https://raw.githubusercontent.com/yebekhe/cf-clean-ip-resolver/main/list.json")
-    while not response.is_finished:
-        sleep(1)
-        Clock.tick()
 
-    json_data = response.result
-    data = json.loads(json_data)
-    matching_ips = []
-    for ipv4_address in data["ipv4"]:
-        if ipv4_address["operator"] == operator_code:
-            matching_ips.append(ipv4_address["ip"])
-    matching_count = len(matching_ips)
-    if matching_count > 0:
-        random_index = random.randint(0, matching_count - 1)
-        random_ip = matching_ips[random_index]
-        return random_ip
-    else:
-        return None
+    def _start_tunnel(self):
+        while self.condition_of_tunnel == True :
+            self.my_socket_timeout = 60
+            self.first_time_sleep = 0.01
+            self.accept_time_sleep = 0.01
+            self.listen_PORT = int(self.screen.ids.local_port_input.text)
+            user_operator_full = self.screen.ids.operator_dropdown.current_item
+            operators = {
+                "Automatic" : "AUTO",
+                "Hamrah-Aval": "MCI",
+                "Irancell": "MTN",
+                "Rightel": "RTL",
+                "Mokhaberat": "MKH",
+                "HiWeb": "HWB",
+                "AsiaTech": "AST",
+                "Shatel": "SHT",
+                "ParsOnline": "PRS",
+                "MobinNet": "MBT",
+                "Andishe-Sabz-Khazar": "ASK",
+                "Respina": "RSP",
+                "AfraNet": "AFN",
+                "Zi-Tel": "ZTL",
+                "Pishgaman": "PSM",
+                "Araax": "ARX",
+                "SamanTel": "SMT",
+                "FanAva": "FNV",
+                "DidebanNet": "DBN",
+                "ApTel": "APT",
+                "Fanap-Telecom": "FNP",
+                "RayNet": "RYN"
+            }
+            self.user_operator = operators.get(user_operator_full)
+            self.Cloudflare_port = int(self.screen.ids.config_port_input.text)
+            print(f'Proxy server listening on 127.0.0.1:{self.listen_PORT}')
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
+                server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                server_sock.bind(('', self.listen_PORT))
+                server_sock.listen(128)
+
+                with ThreadPoolExecutor(max_workers=128) as executor:
+                    while True:
+                        client_sock, client_addr = server_sock.accept()
+                        client_sock.settimeout(self.my_socket_timeout)
+                        time.sleep(self.accept_time_sleep)
+                        executor.submit(self.my_upstream, client_sock)
+
+
 
 
 MainApp().run()
